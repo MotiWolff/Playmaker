ebd6ad61e45007facc85b965b1f2208c848b96c5
David-396 <david.eini12@gmail.com>
2025-09-14 16:14:41 +0300

    fixes

--- services/data_loader/data_loader.py
+++ services/data_loader/data_loader.py
@@ -1,3 +1,4 @@
+import os
 from io import StringIO
 import requests
 import pandas as pd
@@ -6,13 +7,16 @@ import pandas as pd
 class DataLoader:
 
     @staticmethod
-    def download(url:str):
+    def download(url:str, saved_file_path:str):
         try:
+            if os.path.exists(saved_file_path):
+                print(f'file "{saved_file_path}" already exist.')
+                return
 
             data = requests.get(url).content.decode()
             df = pd.read_csv(StringIO(data))
 
-            DataLoader.save_to_csv(file_path=r"C:\Users\User\Desktop\table_1.csv", df=df)
+            DataLoader.save_to_csv(file_path=saved_file_path, df=df)
 
         except Exception as e:
             print(e)
