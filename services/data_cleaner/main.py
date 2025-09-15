6a2bf85336e4b394e542cc567d127dd8077e5289
YosefGu <yos770yos@gmail.com>
2025-09-15 17:46:59 +0300

    Add logger

--- services/data_cleaner/main.py
+++ services/data_cleaner/main.py
@@ -3,12 +3,23 @@ load_dotenv()
 import os
 import uvicorn
 from server import app
+from pathlib import Path
+import sys
+base_path = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(base_path))
 
+from shared.logging.logger import Logger
+
+my_logger = Logger.get_logger()
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "server:app",
-        host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", 8000)),
-        reload=True
-    )
+    try: 
+        uvicorn.run(
+            "server:app",
+            host=os.getenv("HOST", "0.0.0.0"),
+            port=int(os.getenv("PORT", 8000)),
+            reload=True
+        )
+        my_logger.info(f"Clean server is up on port:{os.getenv("PORT")}")
+    except Exception as e:
+        my_logger.error(f"Error running clean server.\nError:{e}")
